ca85854b3f9d74aad243ac6256ddab4bcdecf9f1
Cezary92 <czaro246@gmail.com>
2023-12-04 21:09:53 +0100

    add simple view

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class Main {
+import View.SecurityView;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args){
+
+        SwingUtilities.invokeLater(() -> new SecurityView());
+
+
+        System.out.println("hello");
+    }
 }
